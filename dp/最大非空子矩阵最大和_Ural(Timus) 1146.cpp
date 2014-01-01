5344d4b39274cf988dab10b6b59ae773808b4e25
lanmaoac <acsongyu@gmail.com>
2014-01-01 15:09:04 +0800

    dp

---
+++ dp/最大非空子矩阵最大和_Ural(Timus) 1146.cpp
@@ -0,0 +1,14 @@
+#include <bitset>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int n;
+int num[105][105];
+int sum[105][105];
+int main()
+{   int n;cs
+	return 0; 
+}
