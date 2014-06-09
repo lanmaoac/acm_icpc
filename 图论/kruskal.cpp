3e425b8e3b5ee8c515eac1128cbe36ed11b5619a
lanmao <acsongyu@gmail.com>
2014-06-09 11:27:41 +0800

    grapy

---
+++ 图论/kruskal.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <string.h>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+using namespace std;
+struct node
+{
+  int  to;
+  int from;
+  int w;
+}edge[1000*1000+100];
+int main()
+{ 
+  
+  return 0;
+}
